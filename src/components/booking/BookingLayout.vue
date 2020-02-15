cac691d1f6a25443ee4c12808598d3741eab140a
andrey_platonov <xottabbich@mail.ru>
2020-02-15 22:16:17 +0600

    Clean up initial setup and start routing

---
+++ src/components/booking/BookingLayout.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>Booking Journey</h1>
+    <div>
+      <router-view />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+};
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-import Home from '../views/Home.vue';
+import Home from '../components/Home.vue';
+import BookingLayout from '../components/booking/BookingLayout.vue';
+import Rooms from '../components/booking/rooms/Rooms.vue';
 
 Vue.use(VueRouter);
 
@@ -11,16 +13,21 @@ const routes = [
     component: Home,
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+    path: '/booking',
+    name: 'BookingLayout',
+    component: BookingLayout,
+    children: [
+      {
+        path: 'rooms',
+        name: 'Rooms',
+        component: Rooms,
+      },
+    ],
   },
 ];
 
 const router = new VueRouter({
+  mode: 'history',
   routes,
 });
 

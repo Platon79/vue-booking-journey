79dbe3d72b05d4b30fc654923e2bd63a5f09879a
andrey_platonov <xottabbich@mail.ru>
2020-02-16 19:13:42 +0600

    Create scss skeleton

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,6 +1,9 @@
 <template>
-  <div>
-    Welcome
+  <div class="homepage">
+    <h1>Welcome to the Booking Journey</h1>
+    <router-link to="/booking/rooms" class="btn">
+      Start
+    </router-link>
   </div>
 </template>
 
